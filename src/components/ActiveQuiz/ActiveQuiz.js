0373b992e356fef3c081255e044157e242a38a98
kraevskiy <ilya.labelplatform@gmail.com>
2020-03-29 20:50:28 +0300

    add functionals click/check unswer/next answer

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import classes from './ActiveQuiz.module.css'
+import AnswersList from "./AnswersList/AnswersList";
 
 const ActiveQuiz = props => (
 	<div className={classes.ActiveQuiz}>
 		<p className={classes.Question}>
 			<span>
-				<strong>2. </strong>
-				Как дела?
+				<strong>{props.answerNumber}. </strong>
+				{props.question}
 			</span>
-			<small>4 из 12</small>
+			<small>{`${props.answerNumber} из ${props.quizLenght}`}</small>
 		</p>
-		<ul>
-			<li>1</li>
-			<li>2</li>
-			<li>3</li>
-			<li>4</li>
-		</ul>
+		<AnswersList
+			state={props.state}
+			answers={props.answers}
+			onAnswerClick={props.onAnswerClick}
+		/>
 	</div>
 )
 
