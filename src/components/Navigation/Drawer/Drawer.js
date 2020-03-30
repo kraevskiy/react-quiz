487fd1f152d585a2eb2c697be35238c49a02c173
kraevskiy <ilya.labelplatform@gmail.com>
2020-03-30 12:59:58 +0300

    create NavLink / clickHandler from Drawer.js

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,24 +1,39 @@
 import React, {Component} from 'react';
 import classes from './Drawer.module.css'
 import Backdrop from "../../UI/BackDrop/BackDrop";
+import {NavLink} from 'react-router-dom'
 
-const links = [1,2,3]
+const links = [
+	{to: '/', label: 'Список тестов', exact: true},
+	{to: '/auth', label: 'Авторизация', exact: false},
+	{to: '/quiz-creator', label: 'Создать тест', exact: false},
+]
 
 class Drawer extends Component {
-	renderLinks(){
-		return links.map((link, index)=>{
+	clickHandler = () => {
+		this.props.onClose()
+	}
+	renderLinks() {
+		return links.map((link, index) => {
 			return (
-				<li
-					key={index}
-				>
-					<a>Link {link}</a>
+				<li>
+					<NavLink
+						key={index}
+						to={link.to}
+						exact={link.exact}
+						activeClassName={classes.active}
+						onClick={this.clickHandler}
+					>
+						{link.label}
+					</NavLink>
 				</li>
 			)
 		})
 	}
+
 	render() {
 		const cls = [classes.Drawer]
-		if(!this.props.isOpen){
+		if (!this.props.isOpen) {
 			cls.push(classes.close)
 		}
 		return (
