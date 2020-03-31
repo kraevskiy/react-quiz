4289eaa5f0ce41917b11b00ee72c3fdccaece9d8
kraevskiy <ilya.labelplatform@gmail.com>
2020-03-31 11:10:18 +0300

    add select

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import classes from './QuizCreator.module.css'
 import Button from "../../components/UI/Button/Button"
-import Input from "../../components/UI/Input/Input";
+import Input from "../../components/UI/Input/Input"
+import Select from "../../components/UI/Select/Select"
 import {createControl} from '../../form/formFramework'
-import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
+import Auxiliary from "../../hoc/Auxiliary/Auxiliary"
 
 function createOptionControl(number) {
 	return createControl({
@@ -15,6 +16,7 @@ function createOptionControl(number) {
 
 function createFormControls() {
 	return {
+		rightAnswerId: 1,
 		question: createControl({
 			label: 'Введите вопрос',
 			errorMessage: 'Вопрос не может быть пустым'
@@ -71,7 +73,24 @@ class QuizCreator extends Component {
 		})
 	}
 
+	selectChangeHandler = event => {
+		this.setState({
+			rightAnswerId: +event.target.value
+		})
+	}
+
 	render() {
+		const select = <Select
+			label="Выберите правильный ответ"
+			value={this.state.rightAnswerId}
+			onChange={this.selectChangeHandler}
+			options={[
+				{text: 1, value: 1},
+				{text: 2, value: 2},
+				{text: 3, value: 3},
+				{text: 4, value: 4}
+			]}
+		/>
 		return (
 			<div className={classes.QuizCreator}>
 				<div>
@@ -81,7 +100,8 @@ class QuizCreator extends Component {
 
 						{this.renderControls()}
 
-						<select></select>
+						{select}
+
 						<Button
 							type="primary"
 							onClick={this.addQuestionHandler}>
